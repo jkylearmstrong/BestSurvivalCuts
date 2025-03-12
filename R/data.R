eeff4e115bf940816c3615d18fd73d2355aa3561
J Kyle Armstrong, PhD <j.kyle.armstrong@gmail.com>
2025-03-12 15:34:14 +0000

    data added to package. Please see:
    https://r-pkgs.org/data.html
    
    for details.

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Example Data
+#'
+#' A small example dataset
+#'
+#' @format ## `exdata`
+#' A data frame with 30 rows and 5 columns:
+#' \describe{
+#'   \item{BMI}{body mass index}
+#'   \item{event}{death (0=alive, 1=dead)}
+#'   \item{OS}{time of observation in months (minimum of event time and right censoring time)}
+#'   \item{invasion}{depth of tumor invasion into stroma)}
+#'   \item{LVSI}{lymphovascular space invasion (0=not seen, 1=present)}
+#' }
+"exdata"
+
+#' Toy Data
+#'
+#' A small example dataset
+#'
+#' @format ## `toydata`
+#' A data frame with 537 rows and 5 columns:
+#' \describe{
+#'   \item{Age}{Subject Age}
+#'   \item{BMI}{body mass index}
+#'   \item{stage3}{binary (0=no, 1 = yes)}
+#'   \item{Death}{death (0=alive, 1=dead)}
+#'   \item{Death_surtime}{survival time}
+#' }
+"toydata"
+
+
